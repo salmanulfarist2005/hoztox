05ac2202fa8f27e40aa1d0aaf42ce3475f639f9c
salman <salmansalman79091@gmail.com>
2025-11-21 15:49:20 +0530

    Fix gunicorn version1

--- thavakkal/settings.py
+++ thavakkal/settings.py
@@ -186,7 +186,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.parse(config('DATABASES_URL'))
+DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL '))
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
