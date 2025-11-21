33f93fd8a8750bd788c0f7163b4a95acb1c06326
salman <salmansalman79091@gmail.com>
2025-11-21 15:57:10 +0530

    Fix gunicorn version11

--- thavakkal/settings.py
+++ thavakkal/settings.py
@@ -175,16 +175,16 @@ WSGI_APPLICATION = 'thavakkal.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': config('DATABASE_ENGINE'),
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': config('DATABASE_ENGINE'),
+#         'NAME': config('DATABASE_NAME'),
+#         'USER': config('DATABASE_USER'),
+#         'PASSWORD': config('DATABASE_PASSWORD'),
+#         'HOST': config('DATABASE_HOST'),
+#         'PORT': config('DATABASE_PORT'),
+#     }
+# }
 
 DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL '))
 # Password validation
