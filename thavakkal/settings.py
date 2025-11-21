5f7da07c7002e188282e70bd620196a83d9cc31c
salman <salmansalman79091@gmail.com>
2025-11-21 17:21:07 +0530

    add allowed host

--- thavakkal/settings.py
+++ thavakkal/settings.py
@@ -237,7 +237,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://shop.caratreediamonds.com",
     "https://master.d3btcpc72guulu.amplifyapp.com",
     "https://main.d1c5fqrayqg473.amplifyapp.com",
-    "https://portal.caratreediamonds.com"
+    "https://portal.caratreediamonds.com",
+    "https://hoztox-o4rf.onrender.com"
     
    
 ]
